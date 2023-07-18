9cd20440fc49e1a04f7ad6e5f45cfc4dd742ce75
mohdsammu <113244802+mohdsammu@users.noreply.github.com>
2023-07-18 19:43:45 +0530

    Add files via upload

---
+++ Day5_B79.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # continuation with list datatype
+
+# In[1]:
+
+
+#organising list datatype
+
+
+# In[2]:
+
+
+cars = ['suzuki','maruthi','honda','kia','bmw','toyota','audi','benz']
+
+
+# In[3]:
+
+
+print(cars)
+
+
+# In[4]:
+
+
+type(cars)
+
+
+# In[5]:
+
+
+#i want to organise the list in a alphabatical order A - Z
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+print(sorted(cars))
+
+
+# In[7]:
+
+
+cars.sort()
+
+
+# In[8]:
+
+
+print(cars)
+
+
+# In[10]:
+
+
+# req : i want to print the above list in a reverse order......!
+
+
+# In[11]:
+
+
+cars.reverse()
+
+
+# In[12]:
+
+
+print(cars)
+
+
+# In[13]:
+
+
+#i want to  calculate the no. of elements in the list....?
+
+
+# In[14]:
+
+
+len(cars)
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+students = ['aditi','sara','mounika','ramya','naveen','adil','kiran','david']
+
+
+# In[16]:
+
+
+print(students)
+
+
+# In[17]:
+
+
+print(students[0:1])
+
+
+# In[18]:
+
+
+print(students[0:2])
+
+
+# In[19]:
+
+
+print(students[2:3])
+
+
+# In[20]:
+
+
+print(students[0:8:2])
+
+
+# In[21]:
+
+
+print(students[0:8:3])
+
+
+# In[ ]:
+
+
+# negative indexing
+
+
+# In[22]:
+
+
+print(students)
+
+
+# In[23]:
+
+
+print(students[-1])
+
+
+# In[ ]:
+
+
+
+
